2c14421c51a484c824d51ded4e6b1500a752f698
Sébastien BALARD <sebastien.balard@gmail.com>
2022-09-30 14:00:42 +0200

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let version = "0.6.0"
 let moduleName = "SafeRider"
-let checksum = "0bbeea1d53ce00769ea444987daea1c042681418bae4ac5e11ee90dc0397e5e2"
+let checksum = "9d07a9d7165f4d801dbb78d4a6c8c97a359de8bc8210400330c3ef109d5458eb"
 
 let package = Package(
     name: moduleName,
@@ -17,6 +17,17 @@ let package = Package(
             name: moduleName,
             targets: [moduleName]
         )
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMinor(from: "1.9.0")),
+        .package(url: "https://github.com/Swinject/Swinject.git", .upToNextMinor(from: "2.8.0")),
+        .package(url: "https://github.com/Jounce/Surge", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/CombineCommunity/CombineExt.git", .upToNextMinor(from: "1.8.0")),
+        .package(url: "https://github.com/1024jp/GzipSwift.git", .upToNextMinor(from: "5.2.0")),
+        .package(url: "https://github.com/birdrides/mockingbird.git", .upToNextMinor(from: "0.20.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.6.0")),
+        .package(url: "https://github.com/attaswift/BigInt", .upToNextMinor(from: "5.3.0"))
     ],
     targets: [
         .binaryTarget(
