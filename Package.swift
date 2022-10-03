3e526a2983544377d8b8c35e96e3faf754e8ae0f
Sébastien BALARD <sebastien.balard@gmail.com>
2022-10-03 21:28:56 +0200

    try to add dependencies to binary

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "0.6.4"
+let version = "0.6.2"
 let moduleName = "SafeRider"
 let checksum = "d036ec324dbeff1e91a8115957310af69c5b9db3d9ec93fa191277d2bba652f3"
 
@@ -15,7 +15,7 @@ let package = Package(
     products: [
         .library(
             name: moduleName,
-            targets: [moduleName, "SafeRiderWrapper"]
+            targets: ["SafeRiderWrapper"]
         )
     ],
     dependencies: [
@@ -27,10 +27,11 @@ let package = Package(
             name: moduleName,
             url: "https://github.com/liberty-rider/saferider-ios-distribution/releases/download/\(version)/\(moduleName).zip",
             checksum: checksum
-        )
+        ),
         .target(
             name: "SafeRiderWrapper",
             dependencies: [
+                .target(name: moduleName),
                 .product(name: "BigInt", package: "BigInt")
             ],
             path: "Sources")
