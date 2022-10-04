0506e6b78a2de056c2ec423319f6f81a9f3bdb58
Sébastien BALARD <sebastien.balard@gmail.com>
2022-10-04 14:58:57 +0200

    add all dependencies binaries

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "0.6.3"
+let version = "0.6.4"
 let moduleName = "SafeRider"
 let checksum = "d036ec324dbeff1e91a8115957310af69c5b9db3d9ec93fa191277d2bba652f3"
 
@@ -18,11 +18,20 @@ let package = Package(
             targets: ["SafeRiderWrapper"]
         )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMinor(from: "5.3.0"))
-    ],
     targets: [
+        .target(
+            name: "SafeRiderWrapper",
+            dependencies: [
+                .target(name: moduleName),
+                .target(name: "BigInt"),
+                .target(name: "CombineExt"),
+                .target(name: "CryptoSwift"),
+                .target(name: "Gzip"),
+                .target(name: "Surge"),
+                .target(name: "SwiftyBeaver"),
+                .target(name: "Swinject")
+            ],
+            path: "Sources"),
         .binaryTarget(
             name: moduleName,
             url: "https://github.com/liberty-rider/saferider-ios-distribution/releases/download/\(version)/\(moduleName).zip",
@@ -32,12 +41,29 @@ let package = Package(
             name: "BigInt", 
             path: "Artifacts/BigInt.xcframework"
         ),
-        .target(
-            name: "SafeRiderWrapper",
-            dependencies: [
-                .target(name: moduleName),
-                .target(name: "BigInt")
-            ],
-            path: "Sources")
+        .binaryTarget(
+            name: "CombineExt", 
+            path: "Artifacts/CombineExt.xcframework"
+        ),
+        .binaryTarget(
+            name: "CryptoSwift", 
+            path: "Artifacts/CryptoSwift.xcframework"
+        ),
+        .binaryTarget(
+            name: "Gzip", 
+            path: "Artifacts/Gzip.xcframework"
+        ),
+        .binaryTarget(
+            name: "Surge", 
+            path: "Artifacts/Surge.xcframework"
+        ),
+        .binaryTarget(
+            name: "SwiftyBeaver", 
+            path: "Artifacts/SwiftyBeaver.xcframework"
+        ),
+        .binaryTarget(
+            name: "Swinject", 
+            path: "Artifacts/Swinject.xcframework"
+        )
     ]
 )
